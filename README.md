6c360bbf75ea6f2ee83e8a444fe31f5cc49a0102
Quitandir <carnielmoises@gmail.com>
2023-06-22 09:31:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# CineTag
+
+CineTag is a page where you can watch videos from our database and favorite them. 
+
+## About
+
+This project was developed during a course about React. The aim was using React to create CineTag page. A json file with the videos avaliable to interact in the page was given, so we could mock an api to fetch the videos, using My JSON Server.
+
+## Access
+
+You can access the project here: 
+
+
+
+
+
---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,26 @@
+import Banner from 'components/Banner'
+import styles from './Favoritos.module.css'
+import Titulo from 'components/Titulo'
+import { useFavoritoContext } from 'contextos/Favoritos'
+import Card from 'components/Card'
+
+function Favoritos() {
+
+    const { favorito} = useFavoritoContext()
+
+    return (
+        <>
+            <Banner imagem="favoritos" />
+            <Titulo>
+                <h1>Meus favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {favorito.map((video) => (
+                    <Card key={video.id} {...video} />
+                ))}
+            </section>
+        </>
+    )
+}
+
+export default Favoritos
